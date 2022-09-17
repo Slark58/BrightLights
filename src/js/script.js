c1efb5891a4e70f6a3a07a4448e728f766796607
Nikita Lvov <92874721+Slark58@users.noreply.github.com>
2022-09-18 00:02:40 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -107,7 +107,7 @@ const getResource = async (url) => {
     return await res.json();
 };
 
-getResource('https://slark58.github.io/BrightLights')
+getResource('https://slark58.github.io/BrightLights/db.json')
     .then(data => {
         data.forEach(({img, span, info, data, link}) => {
             new cards(img, span, info, data, link, ".slider__wrapper-content").render()
